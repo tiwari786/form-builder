b02023b07e9ec51eb9fac1e167fe23207211efaa
tiwari786 <vipultiwari479@gmail.com>
2025-12-16 12:37:42 +0530

    url update

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI)
         console.log("Database is connected")
     } catch (error) {
-        console.log("Error in connecting DB")
+        console.log("Error in connecting DB", error)
     }
 }
 
